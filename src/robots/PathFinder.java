a2be3c66822c71f1134991ed49985e9dfe009b01
Noah Brabec <noah.brabec@gmail.com>
2021-01-05 10:48:01 -0600

    Create Muckracker class. Implement some iffy scouting. Update gradle version so that we can use the client.

---
+++ src/robots/PathFinder.java
@@ -0,0 +1,15 @@
+package robots;
+
+import battlecode.common.MapLocation;
+
+/**
+ * Figured it might be nice to have this in its own class
+ * Maybe just have this hold all of our shared funcs
+ */
+public class PathFinder {
+
+    // Very good impl
+    public static MapLocation[] findPath(MapLocation destination){
+        return null;
+    }
+}
