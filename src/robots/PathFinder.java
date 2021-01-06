35f4311343cba4dedb03f9e32d8a48528c19efb8
Noah Brabec <noah.brabec@gmail.com>
2021-01-05 21:10:53 -0600

    Basic path finding

--- src/robots/PathFinder.java
+++ src/robots/PathFinder.java
@@ -10,8 +10,44 @@ import battlecode.common.RobotController;
  */
 public class PathFinder {
 
-    // Very good impl
-    public static Direction[] findPath(MapLocation destination, RobotController robotController){
-        return null;
+    /**
+     * Returns the next direction a robot should move
+     * @param destination The ultimate destination
+     * @param source The robots controller. We use this instead of a location in case we want to weight the movements
+     * @return the next step in the path
+     */
+    public static Direction findPath(MapLocation destination, RobotController source) {
+        int dx = destination.x - source.getLocation().x;
+        int dy = destination.y - source.getLocation().y;
+        Direction direction = null;
+
+        //+- NorthWest
+        //++ NorthEast
+        //-- SouthWest
+        //-+ SouthEast
+        //+= North
+        //=- South
+        //=+ East
+        //-= West
+
+        if (dx > 0 && dy < 0) {
+            direction = Direction.NORTHWEST;
+        } else if ( dx > 0 && dy > 0) {
+            direction = Direction.NORTHEAST;
+        } else if ( dx < 0 && dy < 0) {
+            direction = Direction.SOUTHWEST;
+        } else if ( dx < 0 && dy > 0) {
+            direction = Direction.SOUTHEAST;
+        } else if ( dy > 0) {
+            direction = Direction.NORTH;
+        } else if ( dy < 0) {
+            direction = Direction.SOUTH;
+        } else if ( dx > 0) {
+            direction = Direction.EAST;
+        } else {
+            direction = Direction.WEST;
+        }
+
+        return direction;
     }
 }
